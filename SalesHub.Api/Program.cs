7758d1d7026893c13a9e305a975adb409e40a3c2
Dalya Mlouk <dalyamlouk@aol.com>
2023-11-28 08:19:24 +0000

    Add error handling middleware

--- SalesHub.Api/Program.cs
+++ SalesHub.Api/Program.cs
@@ -1,10 +1,17 @@
+using SalesHub.Api.Filters;
+using SalesHub.Api.Middleware;
+using SalesHub.Application;
+using SalesHub.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 
 var app = builder.Build();
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.MapControllers();
